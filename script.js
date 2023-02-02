21b8c199a0c24bd7036ed65ecfdfe6efc55b7b71
smiritimis2 <113242412+smiritimis2@users.noreply.github.com>
2023-02-02 17:22:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function threeSum(arr, target) {
+/*function threeSum(arr, target) {
   // To store the closest sum
     let closestSum = Number.MAX_VALUE;
  
@@ -22,4 +22,59 @@ function threeSum(arr, target) {
     // Return the closest sum found
     return closestSum;
 }
+module.exports = threeSum;*/
+function threeSum(arr, target) {
+
+  // To store the closest sum
+
+    let closestSum = Number.MAX_VALUE;
+
+ 
+
+    // Run three nested loops each loop
+
+    // for each element of triplet
+
+    for(let i = 0; i < arr.length ; i++)
+
+    {
+
+        for(let j =i + 1; j < arr.length; j++)
+
+        {
+
+            for(let k =j + 1; k < arr.length; k++)
+
+            {
+
+                 
+
+                // Update the closestSum
+
+                if (Math.abs(target - closestSum) >
+
+                    Math.abs(target - (arr[i] + arr[j] + arr[k])))
+
+                    closestSum = (arr[i] + arr[j] + arr[k]);
+
+            }
+
+        }
+
+    }
+
+     
+
+    // Return the closest sum found
+
+    return closestSum;
+
+}
+
 module.exports = threeSum;
+
+
+
+
+
+
